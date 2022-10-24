09e39ade5910e6d6d25220296bd424fb477aa1d6
Sunayana Shivanagi <50568458+sunayana26@users.noreply.github.com>
2022-10-24 15:54:34 -0400

    Implemented other models

---
+++ src/model/Admin.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sunayanashivanagi
+ */
+
+   public class Admin extends Person {
+    
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Admin(String role,String name, int age, String gender, String emailId, long phoneNumber, String userName, String password) {
+        super( name,  age,  gender,  emailId,  phoneNumber,  userName,  password);
+        this.role = role;
+    }
+
+    public Admin() {
+    }
+    
+    
+    
+    
+    
+}
